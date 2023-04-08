1dac8a9e2c073e2f5206df055ef2dd9c4429d7e4
Melony Franchini <meltravelz@gmail.com>
2023-04-08 13:32:54 -0600

    Create tables, model & test files

---
+++ db/migrate/20230408192511_create_homework.rb
@@ -0,0 +1,22 @@
+class CreateHomework < ActiveRecord::Migration[7.0]
+  def change
+    create_table :homeworks do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :language
+      t.string :theme_image
+      t.string :word_prompt
+      t.string :sent1
+      t.string :sent2
+      t.string :sent3
+      t.string :sent4
+      t.string :sent5
+      t.string :sent6
+      t.string :sent7
+      t.string :sent8
+      t.string :sent9
+      t.string :sent10
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/requests/api/v1/users/homeworks_request_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Users Homeworks Controller", type: :request do
+  describe "" do
+    # this will test the homework new form action
+    # a users/homework show & index actions too
+
+  end
+end
