1a738adc2e5cb6323fac34798b5206c3ea15af00
Melony Franchini <meltravelz@gmail.com>
2023-04-08 14:00:53 -0600

    Add bcrypt authorization to user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_many :homeworks
 
-  # validates :user_name, presence: true
-  # validates :email, uniqueness: true, presence: true
-  # validates :password_digest, presence: true
+  validates :user_name, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true
 
-  # has_secrure_password
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,22 +5,23 @@ RSpec.describe User, type: :model do
     it { should have_many(:homeworks) }
   end
 
-  # describe "validations" do
-  #   it { should validate_presence_of(:name) }
-  #   it { should validate_presence_of(:email) }
-  #   it { should validate_uniqueness_of(:email) }
-  #   it { should validate_presence_of(:password) }
-  #   it { should have_secure_password }
-  # end
+  describe "validations" do
+    it { should validate_presence_of(:user_name) }
+    it { should validate_uniqueness_of(:user_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should have_secure_password }
+  end
 
   it "exsists & had attributes" do
-    picard = User.create!(user_name: "Jean-Luc-P", email: "captain@uss-enterprise.com", password_digest: "IamCaptain!")
+    picard = User.create!(user_name: "Jean-Luc-P", email: "captain@uss-enterprise.com", password: "IamCaptain!")
     # riker = User.create!(user_name: "W-Riker", email: "number2@uss-enterprise.com", password_digest: "IamNumber2")
     # data = User.create!(user_name: "Data", email: "data@uss-enterprise.com", password_digest: "IamData?")
 
     expect(picard).to be_a(User)
     expect(picard.user_name).to eq("Jean-Luc-P")
     expect(picard.email).to eq("captain@uss-enterprise.com")
-    expect(picard.password_digest).to eq("IamCaptain!")
+    expect(picard.password).to eq("IamCaptain!")
   end
 end
