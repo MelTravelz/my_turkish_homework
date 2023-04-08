ea50c90918259becd0d4e6e52c4c9b9bc3e25625
Melony Franchini <meltravelz@gmail.com>
2023-04-08 11:12:36 -0600

    Initial commit

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,6 @@
+# Unsplash.configure do |config|
+#   config.application_access_key = ENV["unsplash_access_key"]
+#   config.application_secret = ENV["unsplash_secret"]
+#   # config.application_redirect_uri = "https://your-application.com/oauth/callback"
+#   # config.utm_source = "my_turkish_homework_app"
+# end
