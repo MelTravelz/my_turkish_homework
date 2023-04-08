d7351cbddcdbaae67a752349fbfd10159fc0fa43
Melony Franchini <meltravelz@gmail.com>
2023-04-08 14:09:16 -0600

    Add exsists test to homework model spec

--- spec/models/homework_spec.rb
+++ spec/models/homework_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe Homework, type: :model do
     it { should validate_presence_of :word_prompt }
   end
 
+  it "exsists & had attributes" do
+    picard = User.create!(user_name: "Jean-Luc-P", email: "captain@uss-enterprise.com", password: "IamCaptain!")
+    homework1 = picard.homeworks.create!(language: "Turkish", theme_image: "https://images.unsplash.com/photo-1", word_prompt: "gitmek", sent1: "Haziran Türkiye'ye gidiyorum.")
+  
+    expect(homework1).to be_a(Homework)
+    expect(homework1.language).to eq("Turkish")
+    expect(homework1.theme_image).to eq("https://images.unsplash.com/photo-1")
+    expect(homework1.word_prompt).to eq("gitmek")
+    expect(homework1.sent1).to eq("Haziran Türkiye'ye gidiyorum.")
+  end
 end
