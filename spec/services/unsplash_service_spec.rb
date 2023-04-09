690ffefb5d5343d52e0eb827b71a1855f4967f1a
Melony Franchini <meltravelz@gmail.com>
2023-04-09 10:31:28 -0600

    Begin request spec

--- spec/services/unsplash_service_spec.rb
+++ spec/services/unsplash_service_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe UnsplashService do
   describe "instance methods" do
     context "#fetch_api" do
       it "returns json objects" do
+        image_response = File.read("spec/fixtures/unsplash/random_image.json")
+        stub_request(:get, "https://api.unsplash.com/photos/random?client_id=#{ENV["unsplash_api_key"]}&count=1")
+        .to_return(status: 200, body: image_response, headers: {})
+
         response = UnsplashService.new.fetch_api
         
         expect(response).to be_an(Array)
